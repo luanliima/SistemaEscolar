cfdc9b5a0b7547c80c1ad7d8b8a39306b9c8302d
Luan lima <87495302+luanliima@users.noreply.github.com>
2023-09-26 21:50:47 -0300

    alteração
    criação de uma nova pagina para cadastrar novos alunos

--- main/views.py
+++ main/views.py
@@ -1,8 +1,30 @@
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 # atenção na importação
 
-
+#####################
 def alunoView(request):
     alunos_list = Aluno.objects.all()
     return render(request, 'main/alunos.html', {'alunos_list':alunos_list})
+####################
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request, 'main/alunoID.html',{'aluno':aluno})
+####################
+
+def contact_view(resquest):
+    if resquest.method =='POST':
+        name = resquest.POST.get('name')
+        email = resquest.POST.get('email')
+        message = resquest.POST.get('message')
+        print('name:', name)
+        print('email:', email)
+        print('message:', message)
+    return render(resquest, 'main/contact.html')
+        
+        
+        
+        
+    
